cc6441b53b3ba7c431f1e561b581c33d00b6e712
Peter Trsko <peter.trsko@ixperta.com>
2016-10-14 12:35:27 +0200

    Initial implementation of runPipeServer
    Implementation of runPipeServer is based on streamings-common, and
    Multithreaded Pipe Server example from MSDN:
    
      https://msdn.microsoft.com/en-us/library/windows/desktop/aa365588(v=vs.85).aspx
    
    Hopefuly we got all the details right, but we aren't 100% sure.
    
    Function runPipeServer has its expected type signature:
    
      runPipeServer :: ServerSettingsPipe -> (AppDataPipe -> IO ()) -> IO a
    
    Basic usage example:
    
      runPipeServer (serverSettingsPipe pipeName) $ \appData ->
          -- -->8-- Server code.
    
    Implementation assumptions:
    
    * MSDN example is sufficient, and correct.
    
    * Library streaming-commons does the right thing when it comes to
      Haskell exception handling.
    
    * That it is optimal to use same size of input/output buffer as the read
      size when calling readPipe.
    
    * That preferred pipe mode is (byte) stream, and not message oriented
      delivery. (Currently hardcoded, but we'll be moving this in to
      ServerSettingsPipe) in the near future.
    
    * After bind hook should be executed for each new named pipe. This is
      different from how streaming-commons handles things. This divergence
      is caused by the nature of Named Pipes. To be exact, there is no
      bind-listen protocol, instead, there is a create-wait protocol.

--- src/Data/Streaming/NamedPipes.hs
+++ src/Data/Streaming/NamedPipes.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RecordWildCards #-}
 -- |
 -- Module:       $HEADER$
 -- Description:  Windows Named Pipes streaming API.
@@ -36,6 +37,10 @@ module Data.Streaming.NamedPipes
     )
   where
 
+import Control.Exception (bracket, finally, mask)
+import Control.Concurrent (forkIO)
+import Control.Monad (forever, void, when)
+import Data.Function (($), (.))
 import System.IO (IO)
 
 import Data.Streaming.Network
@@ -44,20 +49,91 @@ import Data.Streaming.Network
     , getReadBufferSize
     , setReadBufferSize
     )
+
 import Data.Streaming.NamedPipes.Internal
     ( AppDataPipe
     , ClientSettingsPipe
     , HasPipeName(pipeNameLens)
     , HasPipePath(pipePathLens)
     , ServerSettingsPipe
+        ( ServerSettingsPipe
+        , serverAfterBindPipe
+        , serverPipeName
+        , serverReadBufferSizePipe
+        )
     , clientSettingsPipe
     , defaultReadBufferSize
+    , mkAppDataPipe
     , serverSettingsPipe
+    )
+import System.Win32.NamedPipes
+    ( PipeHandle
+    , PipeMode(StreamMode)
+    , bindPipe
+    , closePipe
+    , connectPipe
+    , disconnectPipe
+    , readPipe
+    , writePipe
     )
 
 
+-- | Run an server application function with the given settings. This function
+-- will accept connections on a Named Pipe, and spawn a new thread for each
+-- connection.
+--
+-- Example:
+--
+-- @
+-- 'runPipeServer' ('serverSettingsPipe' pipeName) $ \appData ->
+--     -- -->8-- Server code.
+-- @
 runPipeServer :: ServerSettingsPipe -> (AppDataPipe -> IO ()) -> IO a
-runPipeServer = runPipeServer   -- TODO
+runPipeServer cfg@ServerSettingsPipe{..} app = forever . withPipe $ \pipe -> do
+    haveClient <- connectPipe pipe
+    when haveClient $ serve pipe
+  where
+    withPipe :: (PipeHandle -> IO ()) -> IO ()
+    withPipe k = bindPipe' `bracket` closePipe $ \pipe -> do
+        serverAfterBindPipe pipe
+        k pipe
+
+    -- | We are assuming that it is optimal to use same size of input/output
+    -- buffer as the read size when calling readPipe.
+    bindPipe' :: IO PipeHandle
+    bindPipe' = bindPipe serverReadBufferSizePipe StreamMode serverPipeName
+        -- TODO: Move the decision of using specific PipeMode in to
+        --       ServerSettingsPipe.
+
+    -- We need to use closePipe' instead of closePipe, since AppDataPipe are
+    -- living in "app", which means that client is connected to a Named Pipe.
+    -- See closePipe' for details.
+    mkAppData :: PipeHandle -> AppDataPipe
+    mkAppData = mkAppDataPipe cfg readPipe writePipe closePipe'
+
+    -- Implementation of serve is based on how streaming-commons does it,
+    -- i.e. we have masked asynchronous exception during forkIO, and defered
+    -- their apperence to "app" evaluation.
+    serve :: PipeHandle -> IO ()
+    serve pipe = mask $ \restore ->
+        void . forkIO
+            $ restore (app (mkAppData pipe))
+                `finally` closePipe' pipe
+
+    -- When client is already connected to a named pipe, server has to
+    -- disconnect it, which forces client end of the named pipe to be closed,
+    -- before closing the server side end of a named pipe.
+    --
+    -- More can be found in MSDN documentation of DisconnectNamedPipe function:
+    -- https://msdn.microsoft.com/en-us/library/windows/desktop/aa365166(v=vs.85).aspx
+    closePipe' :: PipeHandle -> IO ()
+    closePipe' pipe = do
+        disconnectPipe pipe
+        closePipe pipe
+
+    -- Implementation of runPipeServer is inspired by streamings-common, and
+    -- Multithreaded Pipe Server example from MSDN:
+    -- https://msdn.microsoft.com/en-us/library/windows/desktop/aa365588(v=vs.85).aspx
 
 runPipeClient :: ClientSettingsPipe -> (AppDataPipe -> IO a) -> IO a
 runPipeClient = runPipeClient   -- TODO
