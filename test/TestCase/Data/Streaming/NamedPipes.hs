393f0c0c3c42e1cf08df00cb334fa732848d47c2
Tomas Janousek <tomas.janousek@ixperta.com>
2016-10-26 15:35:23 +0200

    test: Move async/runPipeClient logic to withServer
    This is likely as general as we need it and is easier to use.

--- test/TestCase/Data/Streaming/NamedPipes.hs
+++ test/TestCase/Data/Streaming/NamedPipes.hs
@@ -26,7 +26,7 @@ import Data.Function (($), const)
 import Data.Functor ((<$))
 import System.IO (IO)
 
-import Control.Concurrent.Async (Async, async, waitAnyCancel)
+import Control.Concurrent.Async (async, waitAnyCancel)
 
 import Test.Framework (Test)
 import Test.Framework.Providers.HUnit (testCase)
@@ -35,7 +35,6 @@ import Test.HUnit (Assertion)
 import System.Win32.NamedPipes (PipeName, PipePath(LocalPipe))
 import Data.Streaming.NamedPipes
     ( AppDataPipe
-    , ClientSettingsPipe
     , clientSettingsPipe
     , runPipeClient
     , runPipeServer
@@ -51,16 +50,17 @@ tests =
 withServer
     :: PipeName
     -> (AppDataPipe -> IO ())
-    -> (ClientSettingsPipe -> IO (Async ())) -> IO ()
+    -> (((AppDataPipe -> IO a) -> IO a) -> IO ())
+    -> IO ()
 withServer name serverApp k = do
     server <- async $ runPipeServer (serverSettingsPipe name) serverApp
-    client <- k $ clientSettingsPipe (LocalPipe name)
+    clients <- async $ k (runPipeClient (clientSettingsPipe (LocalPipe name)))
     timeOut <- async $ threadDelay 500000   -- 500 ms
-    () <$ waitAnyCancel [server, client, timeOut]
+    () <$ waitAnyCancel [server, clients, timeOut]
 
 testConnectDisconnect :: Assertion
-testConnectDisconnect = withServer pipeName (const $ pure ()) $ \cfg ->
-    async $ runPipeClient cfg (const $ pure ())
+testConnectDisconnect =
+    withServer pipeName (const $ pure ()) ($ const $ pure ())
 
 pipeName :: PipeName
 pipeName = "win-named-pipes-test"
